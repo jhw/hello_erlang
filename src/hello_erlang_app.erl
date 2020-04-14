f3325a1b37ade15e291a11cd8c891bd1d0223d04
Justin <justin.worrall@gmail.com>
2020-04-14 10:55:24 +0100

    added port config

--- src/hello_erlang_app.erl
+++ src/hello_erlang_app.erl
@@ -9,8 +9,9 @@ start(_Type, _Args) ->
 				      {'_', [{"/echo", echo_handler, []}]},
 				      {'_', [{"/", hello_handler, []}]}
 				     ]),
+    {_, Port}=application:get_env(hello_erlang, port),
     {ok, _} = cowboy:start_clear(my_http_listener,
-				 [{port, 8080}],
+				 [{port, Port}],
 				 #{env => #{dispatch => Dispatch}}
 				),
     hello_erlang_sup:start_link().
